7ccacd23124c0a59a3f284945482894e937750a2
gerardfp <gerardfp@gmail.com>
2019-10-22 19:57:54 +0200

    aa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,22 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    new File("testcases/input").mkdirs();
-	    new File("testcases/output").mkdirs();
+        new File("testcases/input").mkdirs();
+        new File("testcases/output").mkdirs();
 
-	    int testnum = 0;
+        int testnum = 0;
 
-        FileWriter IN = new FileWriter(new File("testcases/input/input"+String.format("%02d", testnum)+".txt"));
-        FileWriter OUT = new FileWriter(new File("testcases/output/output"+String.format("%02d", testnum)+".txt"));
+        for (int v=1; v<15; v++) {
+            FileWriter IN = new FileWriter(new File("testcases/input/input" + String.format("%02d", testnum) + ".txt"));
+            FileWriter OUT = new FileWriter(new File("testcases/output/output" + String.format("%02d", testnum) + ".txt"));
 
-        IN.close();
-        OUT.close();
+
+            IN.close();
+            OUT.close();
+            testnum++;
+        }
+
+        Runtime.getRuntime().exec("zip -r testcases.zip testcases");
+
     }
 }
