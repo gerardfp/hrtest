d45862b9453fe3b8593c2312e5bd0fec3d6ebc60
gerard <gerardfp@gmail.com>
2019-11-15 17:55:04 +0100

    aa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,21 +12,18 @@ public class Main {
         new File("testcases/output").mkdirs();
 
         String[] noms = {"gen","feb","mar","abr","mai","jun","jul","ago","set","oct","nov","des"};
-        int[] d = {160, 140, 130, 110, 80, 70, 60, 30, 70, 100, 130, 150};
+        int[] d = new int[12]; //{160, 140, 130, 110, 80, 70, 60, 30, 70, 100, 130, 150};
+        for (int i = 0; i < 12; i++) {
+            d[i] = (int) (Math.random()*120) + 60;
+        }
         for (int testnum=0; testnum<=6; testnum++) {
             FileWriter IN = new FileWriter(new File("testcases/input/input" + String.format("%02d", testnum) + ".txt"));
             FileWriter OUT = new FileWriter(new File("testcases/output/output" + String.format("%02d", testnum) + ".txt"));
 
             OUT.write("<!DOCTYPE html>\n" +
-                    "<style>\n" +
-                    "div {\n" +
-                    "    display: inline-block;\n" +
-                    "    background-color: blue;\n" +
-                    "    width: 16px;\n" +
-                    "    margin: 2px;\n" +
-                    "}\n" +
-                    "</style>\n");
+                    "<div style=\"display: flex; align-items: baseline;\">\n");
 
+            int[] ms = new int[12];
             for (int y = 1; y <= 12; y++) {
                 int ndies = 0;
                 switch (y) {
@@ -56,16 +53,25 @@ public class Main {
                     q[i] = (int) (Math.random() * (100 + Math.random() * d[y-1]));
                     total += q[i];
                 }
+                ms[y-1] = total/ndies;
 
-                OUT.write("<div style=\"height: "+ total / ndies+"px;\"></div>\n");
-                System.out.println("<div style=\"height: "+ total / ndies+"px;\"></div>" );
                 IN.write(Arrays.toString(q).replace("[", "").replace("]", "") + "\n");
-                System.out.println(Arrays.toString(q).replace("[", "").replace("]", ""));
             }
+
+            int totalmensual = 0;
+            for (int i = 0; i < 12; i++) {
+                totalmensual += ms[i];
+            }
+            int mitjanamensual = totalmensual /12;
+            for (int i = 0; i < 12; i++) {
+                OUT.write("    <div style=\"width: 16px; margin: 2px; background-color: "+ (ms[i] < mitjanamensual ? "red" : "blue") +"; height: "+ ms[i] +"px;\"></div>\n");
+            }
+            OUT.write("    <div style=\"background-color: green; height: 2px; width: 240px; position: relative; top: -" + mitjanamensual + "px; right: 240px\"></div>\n");
+            OUT.write("</div>");
             IN.close();
             OUT.close();
         }
 
-//        Runtime.getRuntime().exec("zip -r testcases.zip testcases");
+        Runtime.getRuntime().exec("zip -r testcases.zip testcases");
     }
 }
--- src/com/company/StanLee.java
+++ src/com/company/StanLee.java
@@ -6,20 +6,49 @@ import java.util.Scanner;
 public class StanLee {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.US);
-        int n = scanner.nextInt();
-        float[] a = new float[n];
 
-        for(int i=0; i<n; i++){
-            a[i] = scanner.nextFloat();
+        scanner.useDelimiter("(, |\\n)");
+
+        int[] mitjanesMensuals = new int[12];
+
+        int totalAnual = 0;
+        for(int i=0; i<12; i++){
+            int ndies = 0;
+            switch (i+1) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    ndies = 31;
+                    break;
+                case 2:
+                    ndies = 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    ndies = 30;
+                    break;
+            }
+            int totalMensual = 0;
+            for(int v=0; v<ndies; v++){
+                totalMensual += scanner.nextInt();
+            }
+            mitjanesMensuals[i] = totalMensual/ndies;
+            totalAnual += mitjanesMensuals[i];
         }
 
-        float m = scanner.nextFloat();
-
-        for(int i=0; i<n; i++){
-            if(a[i] >= m) System.out.println("SI");
-            else System.out.println("NO");
+        int mitjanaAnual = totalAnual/12;
+        System.out.println("<!DOCTYPE html>\n<div style=\"display: flex; align-items: baseline;\">");
+        for(int i = 0; i<12; i++){
+            System.out.println("    <div style=\"width: 16px; margin: 2px; background-color: "+ (mitjanesMensuals[i] < mitjanaAnual ? "red" : "blue") +"; height: "+ mitjanesMensuals[i] +"px;\"></div>");
         }
+        System.out.println("    <div style=\"background-color: green; height: 2px; width: 240px; position: relative; bottom: "+mitjanaAnual +"px; right: 240px\"></div>\n</div>");
+
 
     }
 }
